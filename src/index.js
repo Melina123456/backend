3a1631641a599b42662e1b5d58288dbbbb0c2096
MelinaThapa <thapamelina@redwire.com.np>
2024-01-21 17:10:48 +0545

    all setup configured

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import dotenv from "dotenv";
 
 import connectDB from "./db/db.js";
 
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
