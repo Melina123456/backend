7d5429149504131f19b4625982dc5df4b6f3c786
MelinaThapa <thapamelina@redwire.com.np>
2024-01-22 15:28:17 +0545

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,48 +22,58 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required.");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists.");
   }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required.");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar image is required.");
+  }
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering user.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user created successfully."));
 });
-
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-if (!avatarLocalPath) {
-  throw new ApiError(400, "Avatar image is required.");
-}
-
-const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
-if (!avatar) {
-  throw new ApiError(400, "Avatar image is required.");
-}
-
-const user = await User.create({
-  username,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username: username.toLowerCase(),
-});
-
-const createdUser = await User.findById(user._id).select(
-  "-password -refreshToken"
-);
-
-if (!createdUser) {
-  throw new ApiError(500, "something went wrong while registering user.");
-}
-
-return res
-  .status(201)
-  .json(new ApiResponse(200, createdUser, "user created successfully."));
 
 export { registerUser };
